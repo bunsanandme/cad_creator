5dde9f3cf0cfedc0f78d00edc38b5af79e39e3a1
bunsanandme <bunsanandme@yandex.ru>
2022-03-09 00:26:59 +0300

    First comment

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtWidgets
+import design
+import sys
+
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle("CAD-проектировщик")
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
