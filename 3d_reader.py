8ac2ce45bb7ef68d9cd99726e1f191b856c8787c
bunsanandme <bunsanandme@yandex.ru>
2022-03-09 16:58:51 +0300

    add some new file

---
+++ 3d_reader.py
@@ -0,0 +1,7 @@
+import pyiges
+from pyiges import examples
+
+if __name__ == "__main__":
+    # load an example impeller
+    iges = pyiges.read("T:\my\WORK\cad_creator\\test_data\\test_data.igs")
+
