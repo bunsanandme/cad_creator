941321d2b192cffb1f033c61f379f9edd183a8f6
bunsanandme <bunsanandme@yandex.ru>
2022-03-11 02:44:33 +0300

    add some rendering lines

--- 3d_reader.py
+++ 3d_reader.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyvista as pv
 import pandas as pd
 from io import StringIO
+from math import trunc
 
 
 def convert_xyz_to_array(filename, scale=10):
@@ -10,45 +11,38 @@ def convert_xyz_to_array(filename, scale=10):
     point_cloud = np.loadtxt(StringIO(file_text), skiprows=1, delimiter=',', usecols=(1, 2, 3))
     ijk = np.loadtxt(StringIO(file_text), skiprows=1, delimiter=',', usecols=(4, 5, 6))
     # print(point_names)
-    origin_data = np.around(point_cloud, decimals=3)
+    origin_data = np.floor(point_cloud)
     visualisation_data = origin_data / scale
     return [origin_data, ijk, visualisation_data]
 
 
-def plotting(data, flag='p'):
-    pl = pv.Plotter()
-    pl.add_mesh(data)
-    pl.show()
-
-
-def lod_mesh_export(mesh, lods, extension, path):
-    mesh_lods = {}
-    for i in lods:
-        mesh_lod = mesh.simplify_quadric_decimation(i)
-        o3d.io.write_triangle_mesh(path + "lod_" + str(i) + extension, mesh_lod)
-        mesh_lods[i] = mesh_lod
-    print("generation of " + str(i) + " LoD successful")
-    return mesh_lods
+def lines_from_points(points):
+    """Given an array of points, make a line set"""
+    poly = pv.PolyData()
+    poly.points = points
+    cells = np.full((len(points) - 1, 3), 2, dtype=np.int_)
+    cells[:, 1] = np.arange(0, len(points) - 1, dtype=np.int_)
+    cells[:, 2] = np.arange(1, len(points), dtype=np.int_)
+    cells = np.append(cells, [2, len(points), 0])
+    print(cells)
+    poly.lines = cells
+    return poly
 
 
 if __name__ == "__main__":
-    origin_data = convert_xyz_to_array('T:\\my\\WORK\\cad_creator\\test_data\\data.xyz', 15)
-    polydata = o3d.geometry.PointCloud()
-    polydata.points = o3d.utility.Vector3dVector(origin_data[2])
-    polydata.normals = o3d.utility.Vector3dVector(origin_data[1])
+    origin_data = convert_xyz_to_array('T:\\my\\WORK\\cad_creator\\test_data\\data.xyz', 15)[2][:]
+    polydata = pv.PolyData(origin_data)
+    pl = pv.Plotter()
+    lines = np.array([[2, j, j+1] for j in range(0, 7)])
+    lines = np.append(lines, [2, 7, 0])
+    lines = np.append(lines, [[2, j, j + 1] for j in range(9, 15)])
+    lines = np.append(lines, [2, 15, 9])
+    lines = np.append(lines, [[2, j, j + 1] for j in range(17,20)])
+    lines = np.append(lines, [2, 20, 17])
+    lines = np.append(lines, [[2, j, j + 1] for j in range(23, 41)])
+    lines = np.append(lines, [2, 41, 23])
 
-    poisson_mesh = \
-        o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(polydata, depth=8, width=0, scale=1.1,
-                                                                  linear_fit=False)[0]
-    bbox = polydata.get_axis_aligned_bounding_box()
-    p_mesh_crop = poisson_mesh.crop(bbox)
+    polydata.lines = lines
+    pl.add_mesh(polydata)
+    pl.show()
 
-    o3d.io.write_triangle_mesh("p_mesh_c.ply", p_mesh_crop)
-
-    reader = pv.get_reader("p_mesh_c.ply")
-    mesh = reader.read()
-    mesh.plot()
-    # lines = np.array([[2, j, j+1] for j in range(0, 126)])
-    # print(lines.shape)
-    # polydata.lines = lines
-    # plotting(polydata)
